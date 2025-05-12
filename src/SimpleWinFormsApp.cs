6001341d21a170e0776c0fbe49d7e123192f2c2a
Matheusbarros54 <matheus.barros@sptech.school>
2025-05-12 00:25:43 -0300

    feat: winforms application

---
+++ src/SimpleWinFormsApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace win_form
+{
+    class SimpleWinFormsApp
+    {
+        [STAThread]
+        static void Main()
+        {
+            // Configurações básicas da aplicação
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Criação do formulário
+            Form form = new Form
+            {
+                Text = "Aplicação Simples",
+                Width = 300,
+                Height = 200
+            };
+
+            // Criação do botão
+            Button btnClickMe = new Button
+            {
+                Text = "Clique aqui",
+                Location = new System.Drawing.Point(100, 50),
+                Size = new System.Drawing.Size(100, 30)
+            };
+
+            // Criação do rótulo
+            Label lblMessage = new Label
+            {
+                Location = new System.Drawing.Point(100, 100),
+                AutoSize = true
+            };
+
+            // Evento de clique do botão
+            btnClickMe.Click += (sender, e) => lblMessage.Text = "Olá, mundo!";
+
+            // Adiciona os controles ao formulário
+            form.Controls.Add(btnClickMe);
+            form.Controls.Add(lblMessage);
+
+            // Executa a aplicação
+            Application.Run(form);
+        }
+    }
+}
---
+++ test/SimpleWinFormsAppTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+
+namespace win_form.test
+{
+    [TestClass]
+    public class SimpleWinFormsAppTests
+    {
+        [TestMethod]
+        public void Form_ShouldHaveCorrectTitle()
+        {
+            // Arrange
+            Form form = CreateForm();
+
+            // Act
+            string formTitle = form.Text;
+
+            // Assert
+            Assert.AreEqual("Aplicação Simples", formTitle);
+        }
+
+        private Form CreateForm()
+        {
+            // Simula a criação do formulário no método Main
+            Form form = new Form
+            {
+                Text = "Aplicação Simples",
+                Width = 300,
+                Height = 200
+            };
+
+            Button btnClickMe = new Button
+            {
+                Text = "Clique aqui",
+                Location = new System.Drawing.Point(100, 50),
+                Size = new System.Drawing.Size(100, 30)
+            };
+
+            Label lblMessage = new Label
+            {
+                Location = new System.Drawing.Point(100, 100),
+                AutoSize = true
+            };
+
+            btnClickMe.Click += (sender, e) => lblMessage.Text = "Olá, mundo!";
+
+            form.Controls.Add(btnClickMe);
+            form.Controls.Add(lblMessage);
+
+            return form;
+        }
+    }
+}
